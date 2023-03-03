6e362856ea827a57162bd87b0f1f53b3b27fd469
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-03-04 00:42:38 +0300

    fix import
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
 // package wrapper provides SQL driver wrapper with micro tracing, logging, metering capabilities
-package wrapper // import go.unistack.org/micro-wrapper-sql/v3
+package wrapper // import "go.unistack.org/micro-wrapper-sql/v3"
